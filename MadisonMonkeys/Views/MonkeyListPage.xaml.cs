e0d1d1338c92c0390583ff510608f780520eb2e0
unknown-user <unknown-user@10.0.1.43>
2015-08-22 22:41:20 -0500

    Initial check-in of module MadisonMonkeys

---
+++ MadisonMonkeys/Views/MonkeyListPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using MadisonMonkeys;
+using MadisonMonkeysViewModel;
+
+namespace MadisonMonkeys
+{
+	public partial class MonkeyListPage : ContentPage
+	{
+		MonkeyListViewModel viewModel;
+		public MonkeyListPage ()
+		{
+			InitializeComponent ();
+
+			viewModel = new MonkeyListViewModel();
+
+			ButtonGet.Clicked += async (sender, e) => 
+			{
+				try {
+					ButtonGet.IsEnabled = false;
+					await viewModel.GetMonkeysAsync();
+					ButtonGet.IsEnabled = true;
+				}
+				catch(Exception ex) {
+					DisplayAlert("Oh no!", "Unable to get monkeys:" +ex.Message, "OK");
+				}
+			};
+
+			List.ItemTapped += async (sender, e) =>
+			{
+				var monkey = e.Item;
+				var details = new MonkeyPage();
+				details.BindingContext = monkey;
+
+				await Navigation.PushAsync(details);
+
+				List.SelectedItem = null;
+
+			};
+
+			BindingContext = viewModel;
+		}
+	}
+}
+
---
+++ MadisonMonkeys/Views/MonkeyPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using Refractored.Xam.TTS;
+using Refractored.Xam.TTS.Abstractions;
+
+namespace MadisonMonkeys
+{
+	public partial class MonkeyPage : ContentPage
+	{
+		public MonkeyPage ()
+		{
+			InitializeComponent ();
+
+			OnPressed.Clicked += async (sender, e) => 
+			{
+				
+				CrossTextToSpeech.Current.Speak(TextDetails.Text, false, null, 0.8f, 0.1f, null);
+			};
+
+
+
+		}
+	}
+}
+
